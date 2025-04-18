d291cbc895256e01cde8308db3f7f2962e426f33
Abdelhadisahyouni <52230737@students.liu.edu.lb>
2025-04-18 19:07:22 +0300

    added the new home update

--- Sanidni/src/Pages/Home.jsx
+++ Sanidni/src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import NavBar from '../Components/NavBar';
 import Footer from '../Components/Footer';
 import Notes from '../Components/Notes';
 import Newsletter from '../Components/Newsletter';
+import Welcome from '../Components/Welcome';
 function Home() {
     useEffect(()=>{
 document.body.style.backgroundColor=""
@@ -15,7 +16,9 @@ document.body.style.backgroundColor=""
   return (
 <div className="home ">
 <NavBar></NavBar>
+<Welcome></Welcome>
 <Notes></Notes>
+<Newsletter></Newsletter>
 <Footer></Footer>
 </div>
   )

da7e55c5ef47c7a08df4ae7a0ac0feaf64c1b48f
AbdelhadiSahyouni <52230737@students.liu.edu.lb>
2025-04-27 16:39:47 +0300

    i fixed the about responsive problem

--- src/Components/About2.jsx
+++ src/Components/About2.jsx
@@ -5,12 +5,15 @@ function About2() {
   return (
     <>
 <div className="container-fluid  ">
-<div className="container about2 my-5 d-flex flex-column align-content-center justify-content-center  ">
+<div className="container about2 d-flex flex-column align-content-center justify-content-center">
+
         <p className=" text-start">About Us</p>
-        <p>Helping the heroes of the future to help them build their countries</p>
+        <h3>Helping the heroes of the future to help them build their countries</h3>
+        <button>Start Campaign</button>
         <div className="img-fluid">
           <img src={about} alt="" className="img-fluid" />
         </div>
+        <p></p>
         <p className=''>
           Sandni is a non-profit crowdfunding platform founded in early 2025. Our mission is to support Arab youth by
           helping them return to the educational institutions they belong to, enabling them to contribute to the
